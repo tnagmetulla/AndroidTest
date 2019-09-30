69ebc268742246771cb5995b42c11991d4eaad08
Temirlan Nagmetulla <temirlan.nagmetulla@btsdigital.kz>
2019-09-30 06:34:40 +0600

    refactor and config.yml added

--- app/src/androidTest/java/com/freenow/android_demo/models/TestUser.kt
+++ app/src/androidTest/java/com/freenow/android_demo/models/TestUser.kt
@@ -1,3 +1,7 @@
 package com.freenow.android_demo.models
 
-class TestUser (var username: String, var password:String , var salt:String, var sha256:String)
+class TestUser (
+    var username: String,
+    var password:String ,
+    var salt:String,
+    var sha256:String)
